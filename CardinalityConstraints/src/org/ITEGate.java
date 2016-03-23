2b1295359b28e4b51fb76d82807a5934f2f4341b
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-03-23 11:10:01 +0100

    Erste Idee vom Aufräumen nochmal verbessert

--- CardinalityConstraints/src/org/ITEGate.java
+++ CardinalityConstraints/src/org/ITEGate.java
@@ -1,5 +1,6 @@
 package org;
 
+import com.microsoft.z3.BoolExpr;
 import com.microsoft.z3.Context;
 import com.microsoft.z3.Solver;
 import com.microsoft.z3.Z3Exception;
@@ -45,15 +46,12 @@ public class ITEGate extends Signal {
     // a ist ein Literal.
     public void toZ3(Context ctx, Solver solver, int counter) throws Z3Exception {
 
-        String tVar = "b" + tseitinVar + "_Niklasse_" + counter;
-        String in3Var = "b" + input3.getTseitinVar() + "_Niklasse_" + counter;
+        BoolExpr tVar = ctx.mkBoolConst("b" + tseitinVar + "_Niklasse_" + counter);
+        BoolExpr in3Var = ctx.mkBoolConst("b" + input3.getTseitinVar() + "_Niklasse_" + counter);
+        BoolExpr in1Var = input1.toZ3(ctx);
         String in2Var = "b" + input2.getTseitinVar() + "_Niklasse_" + counter;
 
-        solver.add(ctx.mkOr(
-                ctx.mkNot(ctx.mkBoolConst(tVar)),
-                input1.toZ3(ctx),
-                ctx.mkBoolConst(in3Var))
-        );
+        solver.add(ctx.mkOr(ctx.mkNot(tVar), in1Var, in3Var));
         solver.add(ctx.mkOr(ctx.mkNot(ctx.mkBoolConst(tVar)), ctx.mkNot(input1.toZ3(ctx)), ctx.mkBoolConst(in2Var)));
         solver.add(
                 ctx.mkOr(ctx.mkNot(ctx.mkBoolConst(tVar)),
